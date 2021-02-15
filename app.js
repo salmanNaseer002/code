e9f1537dc850f69d5b1c97c4fd73ad7bbb9ef7c5
salman <salman@gmail.com>
2021-02-15 19:06:33 +0500

    first

--- app.js
+++ app.js
@@ -43,7 +43,17 @@ app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', braintreeRoutes);
-app.use('/api', orderRoutes);
+app.use( '/api', orderRoutes );
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  
+  }
 
 // if ( process.env.NODE_ENV === 'production' )
 // {
