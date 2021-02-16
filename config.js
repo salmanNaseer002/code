de9a422c6b81504b7085aaf9a8614c56b2aebfc3
salman <salman@gmail.com>
2021-02-16 12:27:00 +0500

    change

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ dotenv.config();
   module.exports = {
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://salman:eDC6I6yHY6YUEehu@alphadnd.itop5.mongodb.net/<dbname>?retryWrites=true&w=majority'  //mongodb://localhost/amazona
   
-  }
+}
+// export const BACKEND_URL = process.env.NODE_ENV === 'development'? "http://locahost:8000" : "https://node-react-apppp.herokuapp.com/"
   
